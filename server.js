6e5ad5e2db4e2a6da8bb849905dde7a03553f314
greicens <gsilva@mail.ccsf.edu>
2016-12-15 16:30:58 -0800

    created profile route

--- server.js
+++ server.js
@@ -45,16 +45,28 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/greicens/express-personal-api/blob/master/README.md", // CHANGE ME
+    documentationUrl: "https://github.com/greicens/express-personal-api/blob/master/README.md",
     baseUrl: "http://gsilva.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/projects", description: "Add link to a new project"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Data about me"},
+      {method: "POST", path: "/api/projects", description: "Add link to a new project"}
     ]
   })
+});
+
+app.get('/api/profile', function api_profile(req, res){
+  res.json({
+    name: 'Greice Silva',
+    githubUsername: 'greicens',
+    githubLink: 'https://github.com/greicens',
+    githubProfileImage: 'https://avatars2.githubusercontent.com/u/8587524?v=3&s=400',
+    personalSiteLink: 'https://greicens.github.io/greicens-Project-0.github.io/',
+    currentCity: 'San Francisco',
+    pets: []
+  })
+
 });
 
 /**********
