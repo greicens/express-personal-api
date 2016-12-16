626cb4120f78a4216d7d029f57b057f3a53c7ad1
greicens <gsilva@mail.ccsf.edu>
2016-12-15 16:01:43 -0800

    updated links and endpoints

--- server.js
+++ server.js
@@ -47,12 +47,12 @@ app.get('/api', function api_index(req, res) {
   res.json({
     woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/greicens/express-personal-api/blob/master/README.md", // CHANGE ME
+    baseUrl: "http://gsilva.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "POST", path: "/api/projects", description: "Add link to a new project"} // CHANGE ME
     ]
   })
 });
