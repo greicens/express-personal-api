684f96e1f49b7b3c2ee56a5d2075bf137982d108
greicens <gsilva@mail.ccsf.edu>
2016-12-16 08:57:17 -0800

    fixed the connect url

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/gsilva");
 
 module.exports.Project = require("./project.js");
