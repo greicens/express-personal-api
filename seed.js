f4b34acfbdf5207b455c14043bfb953a62bbfcf1
greicens <gsilva@mail.ccsf.edu>
2016-12-16 17:00:35 -0800

    seed file created with initial data for projects

--- seed.js
+++ seed.js
@@ -1,15 +1,54 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+var db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var project_list = [
+  {
+    {
+      name: "Memory Game"
+      description: "My first game created with GA Fundamentals instructions",
+      githubRepoUrl: "https://github.com/greicens/wdi-fundamentals-memorygame.git",
+      deployedUrl: "",
+      screenshot: ""
+    },
+    {
+      name: "Personal Website",
+      description: "My first website created at GA",
+      githubRepoUrl: "https://github.com/greicens/greicens.github.io.git",
+      deployedUrl: "https://greicens.github.io/",
+      screenshot: "/images/personal_website_pic.png"
+    },
+    {
+      name: "Tic Tac Toe",
+      description: "Simple tic tac toe built using boostrap and event listeners",
+      githubRepoUrl: "https://github.com/greicens/tic-tac-toe.git",
+      deployedUrl: "",
+      screenshot: ""
+    },
+    {
+      name: "Project 0",
+      description: "A racing swimming game built using",
+      githubRepoUrl: "https://github.com/greicens/greicens-Project-0.github.io.git",
+      deployedUrl: "https://greicens.github.io/greicens-Project-0.github.io/",
+      screenshot: ""
 
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
+    }
+];
 
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+db.Book.remove({}, function(err, projects){
+  if(err) {
+    console.log('Error occurred in remove', err);
+  } else {
+    console.log('removed all projects');
+
+    // create new records based on the array books_list
+    db.Book.create(project_list, function(err, books){
+      if (err) {
+        return console.log('err', err);
+      }
+      console.log("created", projects.length, "books");
+      process.exit();
+    });
+  }
+});
