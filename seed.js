01ba09506cb17c27a59eb14d486e04ded199a443
greicens <gsilva@mail.ccsf.edu>
2016-12-16 23:21:50 -0800

    added pictures

--- seed.js
+++ seed.js
@@ -5,49 +5,47 @@ var db = require('./models');
 
 var project_list = [
   {
-    {
-      name: "Memory Game"
-      description: "My first game created with GA Fundamentals instructions",
-      githubRepoUrl: "https://github.com/greicens/wdi-fundamentals-memorygame.git",
-      deployedUrl: "",
-      screenshot: ""
-    },
-    {
-      name: "Personal Website",
-      description: "My first website created at GA",
-      githubRepoUrl: "https://github.com/greicens/greicens.github.io.git",
-      deployedUrl: "https://greicens.github.io/",
-      screenshot: "/images/personal_website_pic.png"
-    },
-    {
-      name: "Tic Tac Toe",
-      description: "Simple tic tac toe built using boostrap and event listeners",
-      githubRepoUrl: "https://github.com/greicens/tic-tac-toe.git",
-      deployedUrl: "",
-      screenshot: ""
-    },
-    {
-      name: "Project 0",
-      description: "A racing swimming game built using",
-      githubRepoUrl: "https://github.com/greicens/greicens-Project-0.github.io.git",
-      deployedUrl: "https://greicens.github.io/greicens-Project-0.github.io/",
-      screenshot: ""
+    name: "Memory Game",
+    description: "My first game created with GA Fundamentals instructions",
+    githubRepoUrl: "https://github.com/greicens/wdi-fundamentals-memorygame.git",
+    deployedUrl: "",
+    screenshot: ""
+  },
+  {
+    name: "Personal Website",
+    description: "My first website created at GA",
+    githubRepoUrl: "https://github.com/greicens/greicens.github.io.git",
+    deployedUrl: "https://greicens.github.io/",
+    screenshot: "/images/personal_website_pic.png"
+  },
+  {
+    name: "Tic Tac Toe",
+    description: "Simple tic tac toe built using boostrap and event listeners",
+    githubRepoUrl: "https://github.com/greicens/tic-tac-toe.git",
+    deployedUrl: "",
+    screenshot: ""
+  },
+  {
+    name: "Project 0",
+    description: "A racing swimming game built using",
+    githubRepoUrl: "https://github.com/greicens/greicens-Project-0.github.io.git",
+    deployedUrl: "https://greicens.github.io/greicens-Project-0.github.io/",
+    screenshot: ""
 
-    }
+  }
 ];
 
-db.Book.remove({}, function(err, projects){
+db.Project.remove({}, function(err, projects){
   if(err) {
     console.log('Error occurred in remove', err);
   } else {
     console.log('removed all projects');
 
-    // create new records based on the array books_list
-    db.Book.create(project_list, function(err, books){
+    db.Project.create(project_list, function(err, projects){
       if (err) {
         return console.log('err', err);
       }
-      console.log("created", projects.length, "books");
+      console.log("created", projects.length, "projects");
       process.exit();
     });
   }
