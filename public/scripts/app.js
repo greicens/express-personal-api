3aadd5af4b12e3274d9f0326f5eed880c4c672b3
greicens <gsilva@mail.ccsf.edu>
2016-12-16 23:20:49 -0800

    render function for handlebars

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,68 @@
 console.log("Sanity Check: JS is working!");
+var template;
+var $projectsList;
+var allProjects = [];
 
 $(document).ready(function(){
 
-// your code
+  $projectsList = $('#projectTarget');
+
+  var source = $('#projects-template').html();
+  template = Handlebars.compile(source);
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/projects',
+    success: onSuccess,
+    error: onError
+  });
+
+  $('#newProjectForm').on('submit', function (target){
+    target.preventDefault();
+    console.log('new project serialized', $(this).serializeArray());
+    $.ajax({
+      method: 'POST',
+      url: 'api/projects',
+      data: $(this).serializeArray(),
+      success: newProjectSucess,
+      error: newProjectError
+    });
+  });
 
 });
+
+function render(){
+  $projectsList.empty();
+  var projectHtml;
+  allProjects.forEach(function(projectData){
+    projectHtml = template({ project: projectData });
+    $projectsList.append(projectHtml);
+  });
+
+}
+
+
+
+function onSuccess(projectData){
+  allProjects = projectData;
+  console.log("allProjects ", allProjects);
+  render();
+
+}
+
+function onError(){
+  console.log('onError error');
+  $('#projectTarget').text('Failed to load projects, is the server working?');
+
+}
+
+function newProjectSucess(json){
+  $('#newProjetForm input').val('');
+  allProjects.push(json);//what does this do?
+  console.log("new project created", json);
+  render();
+}
+
+function newProjectError(){
+  console.log('new project error!');
+}
